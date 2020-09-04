15f79efac6f7e99bb40f2dec1af90aeec28e647a
kevingatera <kgate066@uottawa.ca>
2020-09-04 15:25:03 -0400

    initial commit

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer>
+    <div id="footer">
+      ©2013-2016 BeMo Academic Consulting Inc. All rights reserved.
+      <a
+        href="http://www.cdainterview.com/disclaimer-privacy-policy.html"
+        target="_blank"
+        ><span style="text-decoration:underline;"
+          >Disclaimer &amp; Privacy Policy</span
+        ></a
+      >
+      <a href="mailto:info@bemoacademicconsulting.com" id="rw_email_contact"
+        ><span style="text-decoration:underline;">Contact Us</span></a
+      >
+    </div>
+
+    <div id="socialicons">
+      <div id="socialicons1"></div>
+      <a class="social" href="https://www.facebook.com/bemoacademicconsulting"
+        >F</a
+      ><a class="social" href="https://twitter.com/BeMo_AC">L</a>
+    </div>
+  </footer>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {};
+</script>
+
+<style scoped></style>
---
+++ resources/js/layouts/default.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="main-layout">
+    <navbar />
+
+    <div class="container">
+      <child />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "~/components/Navbar";
+import Footer from "~/components/Footer";
+
+export default {
+  name: "MainLayout",
+
+  components: {
+    Navbar,
+    Footer
+  }
+};
+</script>
+
+<style scoped>
+@import "https://cdainterview.com/rw_common/themes/Endeavor/styles.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/colourtag-page3.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/flexslider.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/contentcenter.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/ec9on.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/rimage.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/ssoff.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/sslide.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/sidenone.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/olight90.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarial.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/title26.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarialspan.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/bts46.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/btoff.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarialnav.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/nav17.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarialside.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarialheader.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarialcontent.css";
+@import "https://cdainterview.com/rw_common/themes/Endeavor/css/font13.css";
+</style>
