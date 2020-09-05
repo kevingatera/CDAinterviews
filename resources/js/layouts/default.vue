3191f560a00eb082990d0aa5f5a3c71c03cfb2dc
kevingatera <kgate066@uottawa.ca>
2020-09-04 22:11:54 -0400

    optimized images

--- resources/js/layouts/default.vue
+++ resources/js/layouts/default.vue
@@ -26,23 +26,23 @@ export default {
 <style scoped>
 @import "../../fromCDA/styles.css";
 @import "../../fromCDA/colourtag-page3.css";
-/* @import "https://cdainterview.com/rw_common/themes/Endeavor/flexslider.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/contentcenter.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/ec9on.css"; */
-/*@import "https://cdainterview.com/rw_common/themes/Endeavor/css/rimage.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/ssoff.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/sslide.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/sidenone.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/olight90.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarial.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/title26.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarialspan.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/bts46.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/btoff.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarialnav.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/nav17.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarialside.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarialheader.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/fontarialcontent.css";
-@import "https://cdainterview.com/rw_common/themes/Endeavor/css/font13.css"; */
+/* @import "../.../fromCDA/flexslider.css";
+@import "../.../fromCDA/css/contentcenter.css";
+@import "../.../fromCDA/css/ec9on.css"; */
+/*@import "../.../fromCDA/css/rimage.css";
+@import "../.../fromCDA/css/ssoff.css";
+@import "../.../fromCDA/css/sslide.css";
+@import "../.../fromCDA/css/sidenone.css";
+@import "../.../fromCDA/css/olight90.css";
+@import "../.../fromCDA/css/fontarial.css";
+@import "../.../fromCDA/css/title26.css";
+@import "../.../fromCDA/css/fontarialspan.css";
+@import "../.../fromCDA/css/bts46.css";
+@import "../.../fromCDA/css/btoff.css";
+@import "../.../fromCDA/css/fontarialnav.css";
+@import "../.../fromCDA/css/nav17.css";
+@import "../.../fromCDA/css/fontarialside.css";
+@import "../.../fromCDA/css/fontarialheader.css";
+@import "../.../fromCDA/css/fontarialcontent.css";
+@import "../.../fromCDA/css/font13.css"; */
 </style>
