d67e4425e1a30b9e94c5086dd82076e2b8515e0b
kevingatera <kgate066@uottawa.ca>
2020-09-06 18:25:04 -0400

    Added functionality to contact form

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Contact;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    // public function getContact() {
+
+    //     return view('contact');
+    //   }
+
+       public function saveContact(Request $request) {
+
+         $this->validate($request, [
+             'name' => 'required',
+             'email' => 'required|email',
+             'message' => 'required'
+         ]);
+
+         $contact = new Contact;
+
+         $contact->name = $request->name;
+         $contact->email = $request->email;
+         $contact->message = $request->message;
+
+         $contact->save();
+
+         Mail::send('contact.contact-us_email',
+             array(
+                 'name' => $request->get('name'),
+                 'email' => $request->get('email'),
+                 'user_message' => $request->get('message'),
+             ), function($message) use ($request)
+               {
+                  $message->from($request->email);
+                  $message->to('kevin@deployitwith.me');
+               });
+
+        //   return back()->with('success', 'Thank you for contact us!');
+        return response()->json([$request->all()]);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+//    Route::post('contact-us', function(Request $request) {
+//        return response()->json([$request->all()]);
+//    });
+
+Route::post('contact-us', 'ContactController@SaveContact');
 
 // Route::get('/Main', function () {
 //     return view('Main');
